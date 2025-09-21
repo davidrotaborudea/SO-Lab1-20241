8cffbcfcc6b7f16045a7438248a37c5cda2280c7
Juan Pablo Zuluaga Jaramillo <Juan.zuluagaj@udea.edu.co>
2025-09-20 22:52:18 -0500

    Reverse con archivos

--- reverse.c
+++ reverse.c
@@ -4,7 +4,18 @@
 #include <string.h>
 
 int main(int argc, char **argv) {
-    if (argc != 1) {
+    (void)argv;
+    FILE *in = NULL;
+
+    if (argc == 1) {
+        in = stdin;
+    } else if (argc == 2) {
+        in = fopen(argv[1], "r");
+        if (!in) {
+            fprintf(stderr, "error: cannot open file '%s'\n", argv[1]);
+            return 1;
+        }
+    } else {
         fprintf(stderr, "usage: reverse <input> <output>\n");
         return 1;
     }
@@ -19,12 +30,13 @@ int main(int argc, char **argv) {
     size_t cap = 0;
     ssize_t n;
 
-    while ((n = getline(&buf, &cap, stdin)) != -1) {
+    while ((n = getline(&buf, &cap, in)) != -1) {
         Node *node = (Node*)malloc(sizeof(Node));
         if (!node) {
             fprintf(stderr, "malloc failed\n");
             free(buf);
             while (head) { Node *nx = head->next; free(head->line); free(head); head = nx; }
+            if (in && in != stdin) fclose(in);
             return 1;
         }
         char *copy = (char*)malloc((size_t)n + 1);
@@ -33,6 +45,7 @@ int main(int argc, char **argv) {
             free(node);
             free(buf);
             while (head) { Node *nx = head->next; free(head->line); free(head); head = nx; }
+            if (in && in != stdin) fclose(in);
             return 1;
         }
         memcpy(copy, buf, (size_t)n);
@@ -46,10 +59,12 @@ int main(int argc, char **argv) {
     for (Node *p = head; p; p = p->next) {
         if (fputs(p->line, stdout) == EOF) {
             while (head) { Node *nx = head->next; free(head->line); free(head); head = nx; }
+            if (in && in != stdin) fclose(in);
             return 1;
         }
     }
 
     while (head) { Node *nx = head->next; free(head->line); free(head); head = nx; }
+    if (in && in != stdin) fclose(in);
     return 0;
 }
